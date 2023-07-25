ce59875100a82fa05c5b3705891c7a40c8fe403d
Desmond Jamal Haynes <desmondjhaynes@gmail.com>
2023-07-25 12:47:53 -0500

    some css changes

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,5 +1,31 @@
+import "./Register.css"
+
+
 export const Register = () => {
+        
     return (
-        <>register here</>
+        <>
+        <div className="Container">
+        <h1>Register Account</h1>
+        <div className="userData">
+        <section className="userImage--container">
+            <div className="preview-Img-container">
+            <img className="image" src="https://res.cloudinary.com/photojam-nss/image/upload/v1690296220/Screenshot_2023-07-17_145151_djqdba.png"/>
+            </div>
+            <div>
+                <label className="upload-label">Upload Profile Photo</label><br/>
+                <input type='file' className="file-upload"/>
+            </div>
+        </section>
+        <section className="userInfo--container">
+                <input type='text' className="user-input" placeholder="User Email" /><br/>
+                <input type='text' className="user-input" placeholder="Full Name " /><br/>
+                <input type='text' className="user-input" placeholder="Username" /><br/>
+                <button className="createAccount button">Create Account</button>
+        </section>
+        </div>
+        </div>
+
+        </>
     )
 }
